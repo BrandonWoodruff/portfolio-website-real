d0ce6967235592f481b89f4d3debd92713b4b890
BrandonWoodruff <bmw132000@gmail.com>
2024-04-04 21:37:49 -0600

    yay

--- portfolio-website/app/routes/_index.tsx
+++ portfolio-website/app/routes/_index.tsx
@@ -1,4 +1,4 @@
-import type { MetaFunction } from "@remix-run/node";
+import { useState } from 'react';
 
 export const meta: MetaFunction = () => {
   return [
@@ -8,21 +8,39 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
+  const [summaryVisible, setSummaryVisible] = useState(false);
+
+  const toggleSummary = () => {
+    setSummaryVisible(!summaryVisible);
+  };
+
+  const highlightSummary = () => {
+    const summaryLink = document.getElementById("summary-link");
+    summaryLink.classList.add("text-purple-400");
+  };
+
+  const unHighlightSummary = () => {
+    const summaryLink = document.getElementById("summary-link");
+    summaryLink.classList.remove("text-purple-400");
+  }
+
   return (
     <div className="bg-video min-h-screen relative overflow-hidden">
       <video className="absolute top-0 left-0 w-full h-full object-cover z-0" autoPlay loop muted>
         <source src="/video/video-1.mov" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
-      <div className="relative z-10 text-white text-center m-8">
-        {/* <div className="mx-auto max-w-lg bg-white bg-opacity-50 rounded-lg p-8 mt-8"> */}
-        <br></br><br></br>
+      <div className="relative z-10 text-white text-center">
+        <br/><br/>
         <h1 className="text-3xl font-bold underline">Brandon Woodruff</h1>
-        <br></br><br></br>
-        <p className="text-2xl">Welcome to my portfolio website!</p>
-        {/* <img src="/images/brandonwoodruff.jpg" alt="Brandon Woodruff" className="h-80 w-80 mx-auto" /> */}
+        <br/><br/>
+        <div id="summary-link" className="transition duration-150 ease-in-out text-2xl underline cursor-pointer" onClick={toggleSummary} onMouseOver={highlightSummary} onMouseOut={unHighlightSummary}>Summary</div>
+        {summaryVisible && (
+          <div id="summary-content" className="mx-auto max-w-4xl bg-white bg-opacity-50 rounded-lg text-2xl">
+            Experienced IT professional with a focus on computing systems and web server technology. Proficient in software development, adept at resolving diverse issues and delivering outstanding support in dynamic, fast-paced environments. Proficient in crafting and implementing solutions, monitoring systems, and facilitating changes. Specialized in leveraging software development skills to enhance operational efficiency and address complex challenges.
+          </div>
+        )}
       </div>
-      </div>
-    // </div>
+    </div>
   );
 }
