11c4f9939509b73724e1dc61b2b1bff2acf03b08
BrandonWoodruff <bmw132000@gmail.com>
2024-04-08 13:04:40 -0600

    switching to mp4 for smaller size

--- portfolio-website/app/components/_background.tsx
+++ portfolio-website/app/components/_background.tsx
@@ -1,7 +1,7 @@
 export function Background () {
   return (
     <video className="fixed top-0 left-0 w-full h-full object-cover z-0" autoPlay loop muted>
-      <source src="/video/video-1.mov" type="video/mp4" />
+      <source src="/video/video-1.mp4" type="video/mp4" />
       Your browser does not support the video tag.
     </video>
   );
